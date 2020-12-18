b937f1fff21c1b9b7163c4582253877b618c8c5d
Jonattan <jonattanva89@gmail.com>
2020-12-18 11:03:47 -0500

    Color component included

---
+++ src/component/color/__tests__/color.test.js
@@ -0,0 +1,23 @@
+import Color from '../index.jsx'
+
+import {
+    render,
+    screen,
+    fireEvent
+} from '@testing-library/react'
+
+describe('<Color />', function () {
+
+    it('open', function () {
+        render(
+            <Color />
+        )
+
+        const button = screen.getByRole("button")
+        fireEvent.click(button)
+
+        const picker = screen.getByLabelText('color picker dialog')
+        expect(picker.className).toMatch(/visible/)
+    })
+
+})
--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,29 @@ const path = require('path')
 
 module.exports = (_, argv) => {
 	return {
-		mode: argv.mode,
+        mode: argv.mode,
+
 		entry: [
 			'./src/application.jsx'
-		],
+        ],
+
 		output: {
             publicPath: '/',
             filename: 'main.js',
             path: path.resolve(__dirname, `./public/main`)
         },
+
         devServer: {
             contentBase: path.resolve(__dirname, './public')
         },
+
         cache: {
             type: 'filesystem',
             buildDependencies: {
                 config: [ __filename ]
             }
         },
+
 		module: {
 			rules: [{
 				test: /\.css$/i,
