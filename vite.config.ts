f9fab887f09e62e066c10005932aba84c2bc3ce1
Jonattan <jonattanva89@gmail.com>
2022-04-04 14:26:34 -0500

    se incluye el componente 'fab'

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,8 @@ export default defineConfig({
         globals: true,
         environment: "jsdom",
         setupFiles: "./setup.ts",
+        coverage: {
+            reporter: ["lcov"],
+        },
     },
 });
