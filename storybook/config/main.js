028084c6dcda88fdc50f9e735350026374f6360a
Jonattan <jonattanva89@gmail.com>
2022-05-04 12:23:03 -0500

    se actualiza librerías y componentes

--- storybook/config/main.js
+++ storybook/config/main.js
@@ -3,9 +3,7 @@ module.exports = {
         builder: "webpack5",
     },
     stories: [
-        "../../src/assets/templates/**/*.stories.mdx",
         "../../src/assets/components/**/*.stories.mdx",
-        "../../src/assets/templates/**/*.stories.@(js|jsx|ts|tsx)",
         "../../src/assets/components/**/*.stories.@(js|jsx|ts|tsx)",
     ],
     addons: [
