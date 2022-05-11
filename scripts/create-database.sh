87062e33c504630ab310980d3f8fa3cd17ad7e24
Jonattan <jonattanva89@gmail.com>
2022-05-11 15:20:46 -0500

    Configuración de la tarea de validación de la base de datos

--- scripts/create-database.sh
+++ scripts/create-database.sh
@@ -2,6 +2,8 @@
 
 createdb \
     --host "$POSTGRES_HOSTNAME" \
+    --port "$POSTGRES_PORT" \
+    --username "$POSTGRES_USER" \
     --no-password \
     --template 'template0' \
     --tablespace 'pg_default' \
