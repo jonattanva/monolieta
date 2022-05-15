dc823ba1a1089337bd534be1f78b2c01f5b1e9a0
Jonattan <jonattanva89@gmail.com>
2022-05-14 21:42:54 -0500

    Se inicia la configuración de los servicios

---
+++ services/monolieta/src/main/kotlin/com/monolieta/entity/Project.kt
@@ -0,0 +1,31 @@
+package com.monolieta
+
+import java.time.LocalDateTime
+import javax.persistence.*
+
+@Entity
+@Table(name = "projects")
+open class Project(
+
+    @field:Id
+    @field:Column(name = "id")
+    open val id: Long? = null,
+
+    @field:Column(name = "name", nullable = false)
+    open val name: String,
+
+    @field:Column(name = "path", nullable = false)
+    open val path: String,
+
+    @field:Column(name = "description")
+    open val description: String? = null,
+
+    @field:Column(name = "created_at", nullable = false)
+    open val created: LocalDateTime = LocalDateTime.now(),
+
+    @field:Column(name = "updated_at")
+    open val updated: LocalDateTime? = null,
+
+    @field:Column(name = "archived", nullable = false)
+    open val archived: Boolean = false
+)
